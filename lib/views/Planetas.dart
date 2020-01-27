f8edeadd8a43f9ab24a7bda1c7e3ee2d8f2dddeb
Mayara Barbosa <mayara.barbosa2@hotmail.com>
2020-01-26 22:43:55 -0300

    assets

--- lib/views/Planetas.dart
+++ lib/views/Planetas.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:galaxy_flutter/views/Login.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 
 class Planetas extends StatefulWidget {
   @override
@@ -7,19 +9,15 @@ class Planetas extends StatefulWidget {
 }
 
 class _PlanetasState extends State<Planetas> {
+
+  var planets = ["Marte","Vênus","Urano","Saturno"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        appBar: AppBar(
         title: Text("Galaxy Flutter", style: TextStyle(color: Colors.white)),
         iconTheme: IconThemeData(color: Colors.white),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app) ,
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
-            },),
-        ],
         backgroundColor: Colors.transparent
         //backgroundColor: Colors.purple[900],
        ),
@@ -30,8 +28,59 @@ class _PlanetasState extends State<Planetas> {
 
       },), 
       backgroundColor: Color(0xff380b4c),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+            itemCount: planets.length,
+            itemBuilder: (context, index) => PlanetRow(planets[index]),
+            padding: EdgeInsets.symmetric(vertical: 4.0)
+              ),
+          ),
+        ],
+      ),
 
+    );
+  }
+}
+
+class PlanetRow extends StatelessWidget {
+  const PlanetRow(this.title);
+
+  final title;
+  
+  final animation = Container(
+     margin: EdgeInsets.symmetric(
+       vertical: 16.0
+     ),
+     alignment: FractionalOffset.centerLeft,
+     child: 
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       height: 120.0,
+       margin: const EdgeInsets.symmetric(
+         vertical: 16.0,
+         horizontal: 24.0,
+       ),
+       //color: Colors.white,
+      child: Stack(
+        children: <Widget>[
+          SizedBox(
+            width: 92,
+            height: 92,
+            child: FlareActor('assets/animations/pinkPlanet.flr',
+              animation: 'rotation',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          //planetCard,
+          //planetThumbnail,
+        ],
+      )
+      
     );
   }
 }
