f4dece252626d4c4e65cd0295c8ee3dad6f077cf
Mayara Barbosa <mayara.barbosa2@hotmail.com>
2020-01-27 01:29:21 -0300

    fazendo a listview

--- lib/views/Planetas.dart
+++ lib/views/Planetas.dart
@@ -10,7 +10,7 @@ class Planetas extends StatefulWidget {
 
 class _PlanetasState extends State<Planetas> {
 
-  var planets = ["Marte","Vênus","Urano","Saturno"];
+  var planets = ["Marte","Vênus","Urano","Saturno","Netuno","Mercúrio"];
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +25,18 @@ class _PlanetasState extends State<Planetas> {
         child: Icon(Icons.add,color: Colors.pink[700],),
         backgroundColor: Colors.white,
         onPressed: (){
-
       },), 
       backgroundColor: Color(0xff380b4c),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-            itemCount: planets.length,
-            itemBuilder: (context, index) => PlanetRow(planets[index]),
-            padding: EdgeInsets.symmetric(vertical: 4.0)
-              ),
+      body: Container(
+        color: Color(0xff380b4c),
+        child: ListView.builder(
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: PlanetRow(planets[index]),
           ),
-        ],
+          itemCount: planets.length,
+          //padding: new EdgeInsets.symmetric(vertical: 16.0)
+        ),
       ),
 
     );
@@ -48,14 +47,6 @@ class PlanetRow extends StatelessWidget {
   const PlanetRow(this.title);
 
   final title;
-  
-  final animation = Container(
-     margin: EdgeInsets.symmetric(
-       vertical: 16.0
-     ),
-     alignment: FractionalOffset.centerLeft,
-     child: 
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -65,22 +56,72 @@ class PlanetRow extends StatelessWidget {
          vertical: 16.0,
          horizontal: 24.0,
        ),
-       //color: Colors.white,
-      child: Stack(
+      child: new Stack(
         children: <Widget>[
-          SizedBox(
-            width: 92,
-            height: 92,
-            child: FlareActor('assets/animations/pinkPlanet.flr',
-              animation: 'rotation',
-              fit: BoxFit.fitWidth,
-            ),
-          ),
-          //planetCard,
-          //planetThumbnail,
+          PlanetCard(),
+          PlanetAnimation(),
         ],
       )
-      
+    );
+    
+  }
+}
+
+class PlanetAnimation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Container(
+        margin: new EdgeInsets.symmetric(
+        vertical: 16.0
+     ),
+     alignment: FractionalOffset.centerLeft,
+     //child: SvgPicture.asset('assets/svg/orbit.svg'),
+     child: Padding(
+       padding: const EdgeInsets.only(right: 300),
+       child: SizedBox(
+         child: FlareActor('assets/animations/pinkPlanet.flr',
+          animation: 'rotation',
+          fit: BoxFit.fitHeight,), 
+          height: 250, 
+          width: 1000,),
+     ),
+
     );
   }
 }
+
+class PlanetCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //height: 124.0,
+      //margin: new EdgeInsets.only(left: 40.0),
+      margin: EdgeInsets.only(left: 40.0, ),
+      decoration:
+      BoxDecoration(
+        color: new Color(0xFF333366),
+        shape: BoxShape.rectangle,
+        borderRadius: new BorderRadius.circular(8.0),
+        boxShadow: <BoxShadow>[
+            BoxShadow(  
+            color: Colors.black12,
+            blurRadius: 10.0,
+            offset: Offset(0.0, 10.0),
+        ),
+      ],
+    ),
+
+    );
+  }
+}
+
+Decoration box = BoxDecoration(
+    gradient: LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    //stops:[0,0.8],
+    //colors: [Colors.purple[400], Colors.purple[800]],
+    colors: [Colors.pink[700], Colors.purple[800]],
+  ),
+        
+);
